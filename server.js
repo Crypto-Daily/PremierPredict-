a565af1c8543dcd9c7c8f29eae1416170ddf108e
CryptoDaily <mfonchrisufan@gmail.com>
2025-08-24 05:30:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.post("/paystack/webhook", async (req, res) => {
   try {
     // 1. Verify Paystack Signature
     const hash = crypto
-      .createHmac("sha512", process.env.sk_live_e854f1ee1be98bd733b97e187aa11f664dd04fc6)
+      .createHmac("sha512", process.env.PAYSTACK_SECRET_KEY)
       .update(JSON.stringify(req.body))
       .digest("hex");
 
