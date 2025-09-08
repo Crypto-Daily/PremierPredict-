40e12ced9a713579f761bff1bc2f009e9cb58136
CryptoDaily <mfonchrisufan@gmail.com>
2025-09-08 20:02:28 +0100

    Update db.js

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // important for Render/Postgres cloud
 });
 
 export default pool;
