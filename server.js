f15361487aac1d73b051b335f9fdf311d3d619c2
CryptoDaily <mfonchrisufan@gmail.com>
2025-09-09 12:08:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import walletRoutes from "./routes/wallet.js";
 import jackpotRoutes from "./routes/jackpot.js";
+import { authMiddleware } from "./middleware/authMiddleware.js";
 
 dotenv.config();
 
@@ -19,3 +20,11 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+const app = express();
+
+app.get("/api/protected", authMiddleware, (req, res) => {
+  res.json({ message: `Hello ${req.user.id}, you have access!` });
+});
+
+export default app;
