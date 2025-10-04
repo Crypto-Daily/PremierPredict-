8659a8e22f18899881bb548b3789ad950780044b
CryptoDaily <mfonchrisufan@gmail.com>
2025-10-04 22:47:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import walletRoutes from "./routes/wallet.js";
 import jackpotRoutes from "./routes/jackpot.js";
 import dashboardRoutes from "./routes/dashboard.js";
 import withdrawalsRouter from "./routes/withdrawals.js";
+import adminRoutes from "./routes/admin.js";
 
 import { authMiddleware } from "./middleware/authMiddleware.js";
 
@@ -38,6 +39,7 @@ app.use("/api/wallet", walletRoutes);
 app.use("/api/jackpot", jackpotRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/withdrawals", withdrawalsRouter);
+app.use("/api/admin", adminRoutes);
 
 /* -------------------------------------------
    🧪 Protected Test Route (optional)
